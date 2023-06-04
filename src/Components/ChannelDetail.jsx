40364b3da4a8224f0348116acc8627b2f8fadf25
Ajay v <vajay4834@gmail.com>
2023-06-04 14:40:42 +0530

    Searche bar completed

--- src/Components/ChannelDetail.jsx
+++ src/Components/ChannelDetail.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import ChannelCard from "./ChannelCard";
+import Videos from "./Video";
+import { fetchFromAPI } from "../utils/FetchFromAPI";
 
 const ChannelDetail = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
 
-export default ChannelDetail
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      try {
+        const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+        setChannelDetail(data?.items[0]);
+
+        const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+        setVideos(videosData?.items);
+      } catch (error) {
+        console.error("Error fetching channel details and videos:", error);
+      }
+    };
+
+    fetchResults();
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        {channelDetail && (
+          <div
+            style={{
+              height: '300px',
+              background: `url(${channelDetail.snippet.thumbnails.high.url})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              zIndex: 10,
+            }}
+          />
+        )}
+        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+      </Box>
+      <Box p={2} display="flex">
+        <Box sx={{ mr: { sm: '100px' } }} />
+        {videos ? (
+          <Videos videos={videos} />
+        ) : (
+          <p>Loading videos...</p>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
--- src/Components/SearchFeed.jsx
+++ src/Components/SearchFeed.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Typography, Box } from '@mui/material';
+import { useParams } from 'react-router-dom';
+
+import { fetchFromAPI } from '../utils/FetchFromAPI';
+import Video from './Video';
 
 const SearchFeed = () => {
-  return (
-    <div>
-      <h1>HEYYYY</h1>
-    </div>
-  )
-}
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
-export default SearchFeed
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchFromAPI(`search?part=snippet&q=${searchTerm}`);
+      setVideos(data.items);
+    };
+
+    fetchData();
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} minHeight="95vh">
+      <Typography variant="h4" fontWeight={900} color="white" mb={3} ml={{ sm: '100px' }}>
+        Search Results for <span style={{ color: '#FC1503' }}>{searchTerm}</span> videos
+      </Typography>
+      <Box display="flex">
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Video videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+
+export default SearchFeed;
